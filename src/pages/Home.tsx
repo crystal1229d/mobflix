f95a7745ec2802667e68c4eff80f38ca038c5e20
crystal1229d <crystal1229d@naver.com>
2024-07-04 15:39:49 +0900

    feat: Home 페이지

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
-import Layout from '@/components/common/Layout'
+import React from 'react'
+
+import MemberList from '@components/club/MemberList'
+import Layout from '@common/Layout'
+import Spacing from '@common/Spacing'
+import FilmList from '@components/film/FilmList'
+import { FILM_LIST_TYPES } from '@/constants'
 
 export default function HomePage() {
-  return <Layout>HomePage</Layout>
+  return (
+    <Layout>
+      <Spacing size={20} />
+      <MemberList />
+      <Spacing size={20} />
+      {FILM_LIST_TYPES.map(({ type, title }) => (
+        <React.Fragment key={type}>
+          <FilmList
+            type={type}
+            title={title}
+          />
+          <Spacing size={50} />
+        </React.Fragment>
+      ))}
+    </Layout>
+  )
 }
